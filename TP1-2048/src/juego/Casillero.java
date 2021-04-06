9ee99d1a4d1646aa6086e69432ac3673ea731591
Mel <Mel@DESKTOP-KOA5OB7>
2021-04-06 19:35:56 -0300

    Arreglo de colores

--- TP1-2048/src/juego/Casillero.java
+++ TP1-2048/src/juego/Casillero.java
@@ -5,54 +5,53 @@ public class Casillero {
 	private int valor;
 	private int posX;
 	private int posY;
-	
-	
-	public Casillero (int valor) {
+
+	public Casillero(int valor) {
 
 		this.valor = valor;
-		
+
 	}
-	
-	public Casillero (int posX, int posY, int valor) {
-		this.valor=valor;
-		this.posX=posX;
-		this.posY=posY;
+
+	public Casillero(int posX, int posY, int valor) {
+		this.valor = valor;
+		this.posX = posX;
+		this.posY = posY;
 	}
 
 	public void setValor(int i) {
 		this.valor = i;
-		
+
 	}
-	
+
 	public int getValor() {
 		return this.valor;
 	}
-	
+
 	public boolean estaVacio() {
-		
-		return this.valor==0; 
+
+		return this.valor == 0;
 	}
-	
-	public void fusionar () {
+
+	public void fusionar() {
 		this.valor += this.valor;
 	}
-	
+
 	public void setPosicion(int x, int y) {
-		this.posX=x;
-		this.posY=y;		
+		this.posX = x;
+		this.posY = y;
 	}
-	
+
 	public int getX() {
 		return this.posX;
 	}
-	
+
 	public int getY() {
 		return this.posY;
 	}
-	
+
 	public void setNumeroRandom() {
-		int random = (int) (Math.random() * 2+1); 
-		setValor(random * 2);		
+		int random = (int) (Math.random() * 2 + 1);
+		setValor(random * 2);
 	}
 
 }
--- TP1-2048/src/juego/Tablero.java
+++ TP1-2048/src/juego/Tablero.java
@@ -4,268 +4,243 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Tablero {
-	private Casillero [][] tablero;
+	private Casillero[][] tablero;
 	private int score = 0;
-	
-	public Tablero() 
-	{		
-		/*crea un tablero de 4x4 de tipo Casillero 
-		   0 1 2 3
-		  0
-		  1
-		  2
-		  3 
-		  
-		  y lo llena con Casilleros en 0
-		
-		*/
-		tablero = new Casillero [4][4];
-		
-		llenarTablero();		
-		
-		//**crea 2 valores randoms entre 2 y 4 en cualquier ubicacio
-		 
+
+	public Tablero() {
+		/*
+		 * crea un tablero de 4x4 de tipo Casillero 0 1 2 3 0 1 2 3
+		 * 
+		 * y lo llena con Casilleros en 0
+		 * 
+		 */
+		tablero = new Casillero[4][4];
+
+		llenarTablero();
+
+		// **crea 2 valores randoms entre 2 y 4 en cualquier ubicacio
+
 		numeroRandomEnPosicionRandom();
-		
+
 		numeroRandomEnPosicionRandom();
 
 	}
 
-	
 	public void llenarTablero() {
 		/*
-		tablero[0][0] = new Casillero(2);
-		tablero[0][1] = new Casillero(2);
-		tablero[0][2] = new Casillero(2);
-		tablero[0][3] = new Casillero(2);
-		tablero[1][0] = new Casillero(2);
-		tablero[1][1] = new Casillero(6);
-		tablero[1][2] = new Casillero(7);
-		tablero[1][3] = new Casillero(8);
-		tablero[2][0] = new Casillero(2);
-		tablero[2][1] = new Casillero(10);
-		tablero[2][2] = new Casillero(11);
-		tablero[2][3] = new Casillero(12);
-		tablero[3][0] = new Casillero(2);
-		tablero[3][1] = new Casillero(14);
-		tablero[3][2] = new Casillero(15);
-		tablero[3][3] = new Casillero(16);
-		*/
+		 * tablero[0][0] = new Casillero(2); tablero[0][1] = new Casillero(2);
+		 * tablero[0][2] = new Casillero(2); tablero[0][3] = new Casillero(2);
+		 * tablero[1][0] = new Casillero(2); tablero[1][1] = new Casillero(6);
+		 * tablero[1][2] = new Casillero(7); tablero[1][3] = new Casillero(8);
+		 * tablero[2][0] = new Casillero(2); tablero[2][1] = new Casillero(10);
+		 * tablero[2][2] = new Casillero(11); tablero[2][3] = new Casillero(12);
+		 * tablero[3][0] = new Casillero(2); tablero[3][1] = new Casillero(14);
+		 * tablero[3][2] = new Casillero(15); tablero[3][3] = new Casillero(16);
+		 */
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
-				tablero[i][j] = new Casillero(0);	
-				//System.out.println( "i:" + i + " "+ "j:" + j + " suma:"+ i+j);
+				tablero[i][j] = new Casillero(0);
+				// System.out.println( "i:" + i + " "+ "j:" + j + " suma:"+ i+j);
 			}
 		}
-		
+
 	}
-	
-	
+
 	public Casillero casilleroRandomVacio() {
-		
+
 		ArrayList<Casillero> posicionesVacias = new ArrayList<Casillero>();
-		
+
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
 				if (tablero[i][j].estaVacio()) {
-					Casillero posicionVacia = new Casillero (i, j, 0);
+					Casillero posicionVacia = new Casillero(i, j, 0);
 					posicionesVacias.add(posicionVacia);
 				}
 			}
 		}
-		if(posicionesVacias.size() >= 1 ) {
+		if (posicionesVacias.size() >= 1) {
 			int posicionRandom = (int) (Math.random() * (posicionesVacias.size() - 1));
 			return posicionesVacias.get(posicionRandom);
 		}
-		
+
 		return null;
-		
+
 	}
-	
-	
+
 	public String obtenerCasillero(int x, int y) {
 		String numerito = Integer.toString(this.tablero[x][y].getValor());
 		return numerito.equals("0") ? "" : numerito;
 	}
-	
-	
+
 	private void numeroRandomEnPosicionRandom() {
 		Casillero primerValor = casilleroRandomVacio();
-		if(primerValor != null) {
-		tablero[primerValor.getX()][primerValor.getY()].setNumeroRandom();
+		if (primerValor != null) {
+			tablero[primerValor.getX()][primerValor.getY()].setNumeroRandom();
 		}
 	}
-	
+
 	private void setScore(int valor) {
 		this.score += valor;
-		
+
 	}
-	
+
 	public int getScore() {
 		return this.score;
 	}
-	
-	
+
 	public void mover(String direccion) {
-		
+
 		int tamanio = 4; // tamanio del tablero
-		
+
 		ArrayList<Integer> eje1 = new ArrayList<Integer>();
 		ArrayList<Integer> eje2 = new ArrayList<Integer>();
-		
+
 		boolean porFila = false;
 		boolean porColumna = false;
-		
+
 		int fila = 0;
 		int columna = 0;
-							
+
 		switch (direccion) {
-		
-			case "arriba":
-				porFila = false;
-				porColumna = true;
-			
-				// setear valores de los ejes
-				for (int n = 0; n < tamanio; n++) {
-					eje1.add(n);
-					eje2.add(n);
-				}			
-				 
-				break;
-				
-			case "abajo":
-				
-				for (int n = 0; n < tamanio; n++) {
-					eje1.add(n);
-				}
-				
-				for (int m = tamanio -1; m >= 0; m--) {
-					eje2.add(m);
-					System.out.println(m);
-				}
-				porFila = false;
-				porColumna = true;
-				
-				break;
-				
-			case "izquierda":		
-					
-				for (int n = 0; n < tamanio; n++) {
-					eje1.add(n);
-					eje2.add(n);
-				}
-				
-				porFila = true;
-				porColumna = false;	
-				
-				break;
-				
-			case "derecha":
-				
-				for (int n = 0; n < tamanio; n++) {
-					eje1.add(n);
-				}
-				
-				for (int m = tamanio -1; m >= 0; m--) {
-					eje2.add(m);
-					System.out.println(m);
-				}
-				
-				porFila = true;
-				porColumna = false;
-				
-				break;
+
+		case "arriba":
+			porFila = false;
+			porColumna = true;
+
+			// setear valores de los ejes
+			for (int n = 0; n < tamanio; n++) {
+				eje1.add(n);
+				eje2.add(n);
+			}
+
+			break;
+
+		case "abajo":
+
+			for (int n = 0; n < tamanio; n++) {
+				eje1.add(n);
+			}
+
+			for (int m = tamanio - 1; m >= 0; m--) {
+				eje2.add(m);
+				System.out.println(m);
+			}
+			porFila = false;
+			porColumna = true;
+
+			break;
+
+		case "izquierda":
+
+			for (int n = 0; n < tamanio; n++) {
+				eje1.add(n);
+				eje2.add(n);
+			}
+
+			porFila = true;
+			porColumna = false;
+
+			break;
+
+		case "derecha":
+
+			for (int n = 0; n < tamanio; n++) {
+				eje1.add(n);
+			}
+
+			for (int m = tamanio - 1; m >= 0; m--) {
+				eje2.add(m);
+				System.out.println(m);
+			}
+
+			porFila = true;
+			porColumna = false;
+
+			break;
 		}
-		
+
 		boolean algunMovimiento = false;
-		
+
 		// Itero segun criterios (ejes - iterador principal y secundario)
 		// Itero sobre el eje principal
 		for (int i1 = 0; i1 < eje1.size(); i1++) {
-			
-			// flag de control para saber si ya finalizaron las operaciones de movimiento y fusion
-			boolean fin = false; 
-			
+
+			// flag de control para saber si ya finalizaron las operaciones de movimiento y
+			// fusion
+			boolean fin = false;
+
 			// indice del casillero por el cual debe comenzarse a iterar
 			// sera util al haber una fusion para evitar multiples fusiones
 			int baseIteracion = 0;
-			
+
 			while (!fin) {
-				
-				boolean seMovio= false;
-				
+
+				boolean seMovio = false;
+
 				// Recorro el eje secundario
-				for (int i2 = baseIteracion; i2 < eje2.size() -1; i2++) {
-					
+				for (int i2 = baseIteracion; i2 < eje2.size() - 1; i2++) {
+
 					fila = porFila ? eje1.get(i1) : eje2.get(i2);
 					columna = porColumna ? eje1.get(i1) : eje2.get(i2);
 
 					System.out.println("C[" + columna + "] F[" + fila + "]: " + tablero[fila][columna].getValor());
-					
+
 					// si itero por fila solo incremento el indice del eje secundario (i2)
-					int filaSiguiente = porFila ? eje1.get(i1) : eje2.get(i2 +1);
-					//si itero por columna solo incremento el indice secundario (i2)
-					int columnaSiguiente = porColumna ? eje1.get(i1) : eje2.get(i2 +1);
-					
+					int filaSiguiente = porFila ? eje1.get(i1) : eje2.get(i2 + 1);
+					// si itero por columna solo incremento el indice secundario (i2)
+					int columnaSiguiente = porColumna ? eje1.get(i1) : eje2.get(i2 + 1);
+
 					if (tablero[fila][columna].estaVacio()) {
-						
+
 						if (!tablero[filaSiguiente][columnaSiguiente].estaVacio()) {
 							tablero[fila][columna].setValor(tablero[filaSiguiente][columnaSiguiente].getValor());
 							tablero[filaSiguiente][columnaSiguiente].setValor(0);
 							seMovio = true;
-							
+
 						}
-						
-					} else if (!tablero[filaSiguiente][columnaSiguiente].estaVacio() 
-							&& tablero[fila][columna].getValor() == tablero[filaSiguiente][columnaSiguiente].getValor()) {
-						
+
+					} else if (!tablero[filaSiguiente][columnaSiguiente].estaVacio() && tablero[fila][columna]
+							.getValor() == tablero[filaSiguiente][columnaSiguiente].getValor()) {
+
 						tablero[fila][columna].fusionar();
 						tablero[filaSiguiente][columnaSiguiente].setValor(0);
-						baseIteracion = i2+1;  
+						baseIteracion = i2 + 1;
 						seMovio = true;
 						setScore(tablero[fila][columna].getValor());
 					}
-					
+
 				}
-				
+
 				if (!seMovio) {
-					fin = true;	
+					fin = true;
 				}
-				
+
 				algunMovimiento = algunMovimiento || seMovio;
 			}
 		}
-		
+
 		if (algunMovimiento) {
 			numeroRandomEnPosicionRandom();
 		}
 
 	}
-	
-	//-----------------------------------------------------------//
-	
-	public void imprimir () {
+
+	// -----------------------------------------------------------//
+
+	public void imprimir() {
 		for (int i = 0; i <= 3; i++) {
 			System.out.println("\n");
 			for (int j = 0; j <= 3; j++) {
 				System.out.print(tablero[i][j].getValor() + " ");
 			}
 		}
-		
-	}
-	
-	
-	
-	
-	
-	
-	public static void main (String [] strg) {
-		Tablero tablero = new Tablero();
-		
-		tablero.imprimir();
+
 	}
 
-	
-	
+	public static void main(String[] strg) {
+		Tablero tablero = new Tablero();
+
+	    //tablero.imprimir();
+	}
 
 }
